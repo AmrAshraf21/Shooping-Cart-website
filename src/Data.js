047cc06079ca77bfaa0cc952d90b8ffed6bce654
AmrAshraf21 <amrashraf314@gmail.com>
2022-07-02 16:56:24 +0200

    first edition

---
+++ src/Data.js
@@ -0,0 +1,102 @@
+let shopItemData = [{
+    id:1,
+    name:'casual',
+    price:45,
+    desc:"lorem salfasfkasfkasokfaos kasokg okasofkaso fasasasfas",
+    img:"images/img-1.jpg"
+
+},
+{
+    id:2,
+    name:'official',
+    price:100,
+    desc:"lorem salfasfkasfkasokfaos kasokg okasofkaso fasasasfas",
+    img:"images/img-2.jpg"
+
+
+
+},
+{
+    id:3,
+    name:'T Shirt',
+    price:80,
+    desc:"lorem salfasfkasfkasokfaos kasokg okasofkaso fasasasfas",
+    img:"images/img-3.jpg"
+
+},
+{
+    id:4,
+    name:'Mens Suit',
+    price:150,
+    desc:"lorem salfasfkasfkasokfaos kasokg okasofkaso fasasasfas",
+    img:"images/img-4.jpg"
+
+},
+{
+    id:5,
+    name:'tie',
+    price:120,
+    desc:"lorem salfasfkasfkasokfaos kasokg okasofkaso fasasasfas",
+    img:"images/img-5.png"
+
+},
+{
+    id:6,
+    name:'Shoes',
+    price:100,
+    desc:"lorem salfasfkasfkasokfaos kasokg okasofkaso fasasasfas",
+    img:"images/img-6.png"
+
+},
+{
+    id:7,
+    name:'Coat',
+    price:80,
+    desc:"lorem salfasfkasfkasokfaos kasokg okasofkaso fasasasfas",
+    img:"images/img-7.png"
+
+},
+{
+    id:8,
+    name:'Polo Shirt',
+    price:50,
+    desc:"lorem salfasfkasfkasokfaos kasokg okasofkaso fasasasfas",
+    img:"images/img-8.png"
+
+},
+
+{
+    id:9,
+    name:'Chemise',
+    price:90,
+    desc:"lorem salfasfkasfkasokfaos kasokg okasofkaso fasasasfas",
+    img:"images/img-9.png"
+
+},
+{
+    id:10,
+    name:'Pants',
+    price:200,
+    desc:"lorem salfasfkasfkasokfaos kasokg okasofkaso fasasasfas",
+    img:"images/img-10.png"
+
+},
+{
+    id:11,
+    name:'Boot',
+    price:300,
+    desc:"lorem salfasfkasfkasokfaos kasokg okasofkaso fasasasfas",
+    img:"images/img-12.png"
+
+},
+{
+    id:12,
+    name:'shirt',
+    price:50,
+    desc:"lorem salfasfkasfkasokfaos kasokg okasofkaso fasasasfas",
+    img:"images/img-3.jpg"
+
+}
+
+
+];
